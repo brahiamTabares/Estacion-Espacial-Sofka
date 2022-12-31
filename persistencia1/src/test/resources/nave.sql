e5c48bb4e581b50b543a64b04282cd48433669ba
BrahiamTabares <bdtabaresv@uqvirtual.edu.co>
2022-12-30 19:45:34 -0500

    se eliminan consultas sql

--- persistencia1/src/test/resources/nave.sql
+++ persistencia1/src/test/resources/nave.sql
@@ -1,10 +1,4 @@
 insert into Nave values("1985",435.0,100,"apolo",2,103.5,204.56,"petroleo","Tripulada")
 insert into Nave values("4567",95,45.39,"Dragon v2",2,43.47,26.89,"nitrogeno","Tripulada");
---insert into Nave values("1959","Mercury","Tripulada","hidracina",120.0,88.0,60.0,53,3)
---insert into Nave values("1989","Explorer","noTripulada","celdas Fotovoltaicas",23.5,15.3,50.5,38,4)
---insert into Nave values("2023","Soho","noTripulada","celfas fotovoltaicas",63.8,50.9,46.1,90,4)
---insert into Nave values("2024","Galileo","noTripulada","celdas fotovoltaicas",80.3,70.9,50.9,80,3)
---insert into Nave values("1866","Saturno","lanzadera","oxido de nitrogeno",150.00,125.00,26.8,250,3)
---insert into Nave values("1897","Atlas","lanzadera","petroleo",133.8,115.00,54.6,150.00,4)
 insert into Nave values("1717",153.8,91.9,"Falcon",4,70.9,103.5,"hidracina","lanzadera");
 
